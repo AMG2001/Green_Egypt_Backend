78882284119c5edc036a332c44dc56b39386322e
Mohamad Daif <mohamad.daif@vodafone.com>
2024-08-30 15:05:57 +0300

    Eco_Friendly_Tips service added.

---
+++ Eco_Friendly_Tips/src/test/java/tech/green_egypt/Eco_Friendly_Tips/EcoFriendlyTipsApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.green_egypt.Eco_Friendly_Tips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoFriendlyTipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- Learn_Testing/src/main/java/Calculator.java
+++
@@ -1,5 +0,0 @@
-public class Calculator {
-    public int integerSubtraction(int minuend, int subtrahend) {
-        return minuend - subtrahend;
-    }
-}
